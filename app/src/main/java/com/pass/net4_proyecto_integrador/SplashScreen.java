57b85bc31dab7f956eb3d7b8e16d467f81908be8
pablodiazsanz <Githubpablo2019>
2021-01-22 18:10:56 +0100

    Splash Screen Terminado y animaciones Splash insertadas

--- app/src/main/java/com/pass/net4_proyecto_integrador/SplashScreen.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/SplashScreen.java
@@ -8,12 +8,40 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.pass.net4_proyecto_integrador.MainActivity;
+
 public class SplashScreen extends Activity {
+
+    private Animation topAnimation, bottomAnimation;
+    private ImageView icon, name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation_splash_screen);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation_splash_screen);
+
+        icon = findViewById(R.id.iv_icon);
+        name = findViewById(R.id.iv_name);
+
+        icon.setAnimation(topAnimation);
+        name.setAnimation(bottomAnimation);
+
+        openApp(true);
+    }
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen
+                        .this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
     }
 }
