273165f9c16f0617eb15eef081970233b37917c5
Alvaro230 <surfiera@gmail.com>
2021-01-22 18:25:10 +0100

    login.xml terminado y botones

---
+++ app/src/main/java/com/pass/net4_proyecto_integrador/LogInActivity.java
@@ -0,0 +1,20 @@
+package com.pass.net4_proyecto_integrador;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+Button btn_login;
+TextView txt_btn_sign_up;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+}
