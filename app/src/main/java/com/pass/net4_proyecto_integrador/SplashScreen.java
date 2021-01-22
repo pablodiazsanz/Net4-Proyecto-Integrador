ee807322f56c325356b81fde79fdf6cd2943d607
pablodiazsanz <Githubpablo2019>
2021-01-22 18:04:08 +0100

    Insertados logos y splash_screen.xml terminado

---
+++ app/src/main/java/com/pass/net4_proyecto_integrador/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.pass.net4_proyecto_integrador;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+    }
+}
