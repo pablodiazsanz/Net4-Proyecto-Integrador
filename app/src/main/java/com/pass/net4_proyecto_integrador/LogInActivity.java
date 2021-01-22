8dfdf9d42eb079f5a44dc881749a9d480cab44af
Alvaro230 <surfiera@gmail.com>
2021-01-22 18:45:59 +0100

    LogIn Terminado

--- app/src/main/java/com/pass/net4_proyecto_integrador/LogInActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/LogInActivity.java
@@ -15,6 +15,25 @@ TextView txt_btn_sign_up;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        btn_login = findViewById(R.id.btn_log_in);
+        txt_btn_sign_up = findViewById(R.id.txt_register);
 
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(com.pass.net4_proyecto_integrador.LogInActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        /*txt_btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
