4cf5dafcd69fa7f3d0bfb2d9cdd97e5c988eb6af
SergyDany47 <sergiodani47@gmail.com>
2021-01-28 02:17:49 +0100

    Rediseño termiando con exito

--- app/src/main/java/com/pass/net4_proyecto_integrador/LoginActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.pass.net4_proyecto_integrador;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,45 +10,43 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LogInActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private Button btn_login;
-    private TextView txt_btn_sign_up;
     private TextView txt_forgot_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_login);
-        btn_login = findViewById(R.id.btn_log_in);
-        txt_btn_sign_up = findViewById(R.id.txt_register);
+        btn_login = findViewById(R.id.btn_login);
         txt_forgot_password = findViewById(R.id.txt_forget);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-            }
-        });
-
-        txt_btn_sign_up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });
         txt_forgot_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAlertDialogButtonClicked(LogInActivity.this);
+                showAlertDialogButtonClicked(LoginActivity.this);
             }
         });
 
     }
-    public void showAlertDialogButtonClicked(LogInActivity view) {
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+    }
+
+    public void showAlertDialogButtonClicked(LoginActivity view) {
         // setup the alert builder
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setView(getLayoutInflater().inflate(R.layout.activity_email_recovery,
