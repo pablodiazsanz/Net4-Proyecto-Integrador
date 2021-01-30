f1cd8c48044b4deee951d032eb5d0a0b16bac55b
Alvaro230 <surfiera@gmail.com>
2021-01-30 17:41:11 +0100

    Empezando Mapa

--- app/src/main/java/com/pass/net4_proyecto_integrador/MainActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/MainActivity.java
@@ -1,14 +1,37 @@
 package com.pass.net4_proyecto_integrador;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    BottomNavigationView bnv;
+    private BottomNavigationView bnv;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location currentlocation;
+    private static final int REQUEST_CODE = 101;
+    private double longitud;
+    private double latitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +41,55 @@ public class MainActivity extends AppCompatActivity {
         bnv = findViewById(R.id.bottomNavigationBar);
         bnv.setBackground(null);
         bnv.getMenu().getItem(2).setEnabled(false);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentlocation = location;
+                    latitud = currentlocation.getLatitude();
+                    longitud = currentlocation.getLongitude();
+                    Log.d("LyL","La latitud:" + latitud + " Longitud:" + longitud);
+                    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MainActivity.this);
+                    fetchLastLocation();
+                }
+            }
+        });
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng ubi = new LatLng(currentlocation.getLatitude(), currentlocation.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(ubi));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ubi,5));
+        mMap.addMarker(new MarkerOptions().position(ubi).title("Mi Ubicacion"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(ubi));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CODE:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fetchLastLocation();
+                }
+                break;
+        }
+    }
+
 }
