254e987ec30a609e4cca80ab9164e63e75d68295
Alvaro230 <surfiera@gmail.com>
2021-02-02 11:37:09 +0100

    Rama Mapa comentada

--- app/src/main/java/com/pass/net4_proyecto_integrador/MainActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/MainActivity.java
@@ -1,15 +1,13 @@
 package com.pass.net4_proyecto_integrador;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -24,11 +22,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private BottomNavigationView bnv;
+    private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap mMap;
     private OnMapReadyCallback omrc = this;
-    private FusedLocationProviderClient fusedLocationClient;
     private Location currentlocation;
     private static final int REQUEST_CODE = 101;
     private double longitud;
@@ -47,29 +44,21 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         fetchLastLocation();
     }
 
-    private void fetchLastLocation() {
+    void fetchLastLocation() {
+        //Aqui prguntaremos si quiere dar permiso de ubicacion a la aplicacion en caso de que no tenga ira al metodo onRequestPermissionsResult(),
+        //si ya tiene los permiso ira directamente a obtenerLocalizacion()
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]
                     {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
-        }else{
+        } else {
             obtenerLocalizacion();
         }
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        LatLng ubi = new LatLng(latitud, longitud);
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(ubi));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ubi, 5));
-        mMap.addMarker(new MarkerOptions().position(ubi).title("Mi Ubicacion"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(ubi));
-    }
-
-    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //comprobamos que le dan permiso a la apliocacion para que coja la ubicacio
         if (requestCode == REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 obtenerLocalizacion();
@@ -78,18 +67,20 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void obtenerLocalizacion() {
+        //Aqui revisamos si estan los permisos
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+        //Aqui recojemos las ultimas ubicaciones
         Task<Location> task = fusedLocationClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                if (location != null){
+                if (location != null) {
                     currentlocation = location;
                     latitud = currentlocation.getLatitude();
                     longitud = currentlocation.getLongitude();
-                    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                    // Para llamar al metodo onMapReady
                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                             .findFragmentById(R.id.map);
                     mapFragment.getMapAsync(omrc);
@@ -98,4 +89,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //Aqui se pasan las coordenadas
+        LatLng ubi = new LatLng(latitud, longitud);
+        //Aqui dirigimos la camara a la ubicacion
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(ubi));
+        //El zoom que va de 0 a 21
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ubi, 10));
+        //Esto es el marcador con el titulo de ubicacion
+        mMap.addMarker(new MarkerOptions().position(ubi).title("Mi Ubicacion"));
+    }
 }
