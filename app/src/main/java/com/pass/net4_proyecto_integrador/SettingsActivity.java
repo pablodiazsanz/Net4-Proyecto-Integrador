ed51ec086933edc7182e7579eb70ce41a581645c
Alvaro230 <surfiera@gmail.com>
2021-02-12 12:17:50 +0100

    Contacto y Politica de privacidad hecho

--- app/src/main/java/com/pass/net4_proyecto_integrador/SettingsActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.pass.net4_proyecto_integrador;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,11 +10,16 @@ import android.widget.LinearLayout;
 
 public class SettingsActivity extends AppCompatActivity {
     private LinearLayout cerrarSesion;
+    private LinearLayout politicaDePrivacidad;
+    private LinearLayout contacto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         cerrarSesion = findViewById(R.id.cerrar_sesion);
+        politicaDePrivacidad = findViewById(R.id.politica_privacidad);
+        contacto = findViewById(R.id.contacto);
+
 
         cerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +30,34 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        politicaDePrivacidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, PDFActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        contacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertDialogContact();
+            }
+        });
+    }
+    /**
+     * In this method, the password recovery screen has been implemented
+     * as an alert dialog.
+     */
+    private void showAlertDialogContact() {
+        // Here we setup the alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(getLayoutInflater().inflate(R.layout.activity_contact,
+                null));
+
+        // Here we create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
