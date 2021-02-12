5933aa4d6fd926d61d8c40aef12303aad593d57c
Alvaro230 <surfiera@gmail.com>
2021-02-12 16:15:27 +0100

    logos cambiados

--- app/src/main/java/com/pass/net4_proyecto_integrador/EmailRecoveryActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/EmailRecoveryActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ContactActivity extends AppCompatActivity {
+public class EmailRecoveryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
