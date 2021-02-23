189e1bb12c9cc7db57d1cafcb7ebe30a8aaa4829
pablodiazsanz <Githubpablo2019>
2021-02-23 23:52:43 +0100

    Splash screen modificado + ciertas modificaciones en la app

--- app/src/main/java/com/pass/net4_proyecto_integrador/mainActivities/profile/ProfileActivity.java
+++ app/src/main/java/com/pass/net4_proyecto_integrador/mainActivities/profile/ProfileActivity.java
@@ -2,16 +2,22 @@ package com.pass.net4_proyecto_integrador.mainActivities.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
+import com.pass.net4_proyecto_integrador.SettingsActivity;
 import com.pass.net4_proyecto_integrador.mainActivities.maps.MapsActivity;
 import com.pass.net4_proyecto_integrador.R;
 import com.pass.net4_proyecto_integrador.mainActivities.dashboard.DashboardActivity;
@@ -62,6 +68,8 @@ public class ProfileActivity  extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabLayout_id);
         appBarLayout = (AppBarLayout) findViewById(R.id.appBar_id);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_profile);
+        setSupportActionBar(myToolbar);
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);
         ProfileAdapterActivity adapter = new ProfileAdapterActivity(getSupportFragmentManager());
         //Adding Fragments
@@ -71,5 +79,21 @@ public class ProfileActivity  extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settings_button){
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile_fragment, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
